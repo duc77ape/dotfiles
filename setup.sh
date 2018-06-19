908af4e525c44cb6dd7ecf897b41a1cce514afab
duc77ape <10351592+duc77ape@users.noreply.github.com>
2018-06-19 23:33:10 +0900

    modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
+
+shopt -s nullglob
+shopt -s extglob
+
 for file in .??*
 do
     [[ "$file" == ".git" ]] && continue
@@ -9,3 +13,8 @@ do
     echo $SCRIPT_DIR/$file
     ln -snf $SCRIPT_DIR/$file ~/$file
 done
+
+for rcfile in submodules/prezto/runcoms/!(README.md); do
+  ln -s $SCRIPT_DIR/$rcfile ~/.${rcfile##*/}
+done
+ln -s $SCRIPT_DIR/submodules/prezto/ ~/.zprezto
