3030193a9ba4eff2446084a46d79940a6323fe3a
duc77ape <duc77ape@protonmail.com>
2019-04-08 16:24:42 +0900

    Updated

--- setup.sh
+++ setup.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
+echo $SCRIPT_DIR
 
 shopt -s nullglob
 shopt -s extglob
 
-for file in .??*
+cd $SCRIPT_DIR
+while read -d $'\0' file;
 do
+    file=$(basename $file)
     [[ "$file" == ".git" ]] && continue
     [[ "$file" == ".gitmodules" ]] && continue
     [[ "$file" == ".gitignore" ]] && continue
     [[ "$file" == ".gitconfig" ]] && continue
+    [[ "$file" == "setup.sh" ]] && continue
+    [[ "$file" == "config" ]] && continue
     echo $SCRIPT_DIR/$file
-    ln -snf $SCRIPT_DIR/$file ~/$file
-done
+    ln -snb $SCRIPT_DIR/$file ~/$file
+done <  <(find . -mindepth 1 -maxdepth 1 -name "*" -print0)
 
-for rcfile in submodules/prezto/runcoms/!(README.md); do
-  ln -s $SCRIPT_DIR/$rcfile ~/.${rcfile##*/}
-  echo $SCRIPT_DIR/$rcfile ~/.${rcfile##*/}
-done
-ln -s $SCRIPT_DIR/submodules/prezto/ ~/.zprezto
-echo $SCRIPT_DIR/submodules/prezto/ ~/.zprezto
+while read -d $'\0' file;
+do
+    file=$(basename $file)
+    echo $SCRIPT_DIR/config/$file
+    ln -snb $SCRIPT_DIR/config/$file ~/.config/$file
+done <  <(find config -mindepth 1 -maxdepth 1 -name "*" -print0)
+
+#for rcfile in submodules/prezto/runcoms/!(README.md); do
+#  ln -s $SCRIPT_DIR/$rcfile ~/.${rcfile##*/}
+#  echo $SCRIPT_DIR/$rcfile ~/.${rcfile##*/}
+#done
+#ln -s $SCRIPT_DIR/submodules/prezto/ ~/.zprezto
+#echo $SCRIPT_DIR/submodules/prezto/ ~/.zprezto
